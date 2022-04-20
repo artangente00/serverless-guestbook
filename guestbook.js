3e7d49e67c7ace05ecf7370fb0b2b8dea204cc96
artangente00 <98647236+artangente00@users.noreply.github.com>
2022-04-20 14:02:39 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://0d8c864a.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
